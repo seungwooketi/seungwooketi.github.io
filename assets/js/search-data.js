39c7743709b2a854346c90b05ec393ead541f7c3
seungwooketi <45585779+seungwooketi@users.noreply.github.com>
2026-05-29 13:15:36 +0000

    Deploying to gh-pages from @ seungwooketi/seungwooketi.github.io@aba0b65ee6e30326c3daf56034191a7823d07e23 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,6 +37,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
+        },{id: "nav-repositories",
+          title: "repositories",
+          description: "Open-source code, prototypes, and tooling.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/repositories/";
+          },
         },{id: "nav-blog",
           title: "blog",
           description: "",
