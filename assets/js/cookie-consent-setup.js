5d6d81b7a54dfbe75d4f1ab71d5e0843e8ad6379
seungwooketi <45585779+seungwooketi@users.noreply.github.com>
2026-07-15 09:22:49 +0000

    Deploying to gh-pages from @ seungwooketi/seungwooketi.github.io@1dc852844f2c22965fe5613709e76b3eef0b5e4d 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://seungwooketi.github.io/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://seungwoo.datasec.work/#contact">contact us</a>.'
               }
             ]
           }
